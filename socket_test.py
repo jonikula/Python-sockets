f08dc729398984644f0e67ef705eeed6e161dcde
Joonas Nikula <jonikula@paju.oulu.fi>
2013-03-08 20:16:27 +0200

    New versions of both programs. Some added content, server now replies to all messages!

--- socket_test.py
+++ socket_test.py
@@ -17,9 +17,22 @@ try:
 except socket.gaierror:
     print 'Hostname could not be resolved. Exiting.'
     sys.exit()
-ip = '192.168.100.41'
-port = 5555
+#ip = '192.168.100.41'
+port = 5557
 s.connect((remote_ip, port))
 print 'Socket connected to ' + host
+while True:
+
+    while True:
+        data = s.recv(1024)
+        print data
+        break
+        
+    msg = raw_input('Type a message')
+    try:
+        s.sendall(msg)
+    except socket.error:
+        print 'Send failed'
+        sys.exit()
 
  
--- socket_test_srvr.py
+++ socket_test_srvr.py
@@ -5,17 +5,8 @@ import sys
 from thread import * 
 
 HOST = ''
-PORT = 5555
-def clientthread(conn):
-    #Vastataan yhteyteen
-    conn.sendall('Saatana mie tahtosin nukkua! Mutta leikkik‰‰!')
-    while True:
-        print 'Connected with host on ' + addr[0] + ':' + str(addr[1])
-        reply = conn.recv(1024)
-        print reply
-        if not reply:
-            break
-    conn.close()
+PORT = 5557
+
     
 #Create socket s
 try:
@@ -23,18 +14,29 @@ try:
 except socket.error, msg:
     print 'Failed to create socket. Error message: ' + str(msg[1])
     sys.exit()
-
+print 'Socket created'
 #Bind socket s to an abritary host and port    
 try:
     s.bind((HOST, PORT))
 except socket.error, msg:
-    print 'Bind failer, error message: ' + msg[1]
+    print 'Bind failed, error message: ' + msg[1]
     sys.exit()
 print 'Bind successfull'
-
+    
 #Set socket to listen for connections
 s.listen(10)
 print 'Socket now listening'
+
+def clientthread(conn):
+    #Vastataan yhteyteen
+    conn.sendall('T‰‰ kertoo juttuja!')
+    while True:
+        reply = conn.recv(1024)
+        print reply
+        if not reply:
+            break
+        conn.sendall('Ok... ' + reply)
+    conn.close()
 
 conn, addr = s.accept() #Accept incoming connection
 while True:
